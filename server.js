f0cc4ab7e3e90a853b8f1ea9727de5ce43dc9dda
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-12-09 17:29:34 -0800

    show key on hover

--- server.js
+++ server.js
@@ -20,14 +20,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/coupons_controller.js");
 
 app.use(routes);
 
-
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+    console.log("🌎  ==> API Server now listening: http://localhost:" + PORT);
 });
 
